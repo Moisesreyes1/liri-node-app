9d6b5de03091a9c9eaac6504d3870bc8edf51d95
unknown <mmreyes13@gmail.com>
2017-10-14 16:16:40 -0400

    Tweets working; movie and spotify working with bugs

--- liri.js
+++ liri.js
@@ -1,115 +1,96 @@
 // Require keys and npm packages
 var keys = require("./keys");
 var twitter = require("twitter");
-var spotify = require("node-spotify-api");
+var Spotify = require("node-spotify-api");
 var request = require("request");
 var fs = require("fs");
 
-// console.log("liri.js");
-// Twitter search function
-function myTweets() {
-	console.log(keys);
-	// var client = new twitter(keys);
-	var client = new twitter(
-		keys
 
-);
-	var params = { screen_name: "mmreyes13"};
-	client.get("statuses/user_timeline", params, function(error, tweets, response) {
+// Twitter function
+var myTweets = function() {
+
+// Twitter keys
+var keyforTwitter = keys;
+
+// NPM twitter package
+var client = new twitter(keyforTwitter);
+var params = { screen_name: 'mmreyes13'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
-    console.log(tweets);
-    console.log(response);
-  }
+
+// Loop thru tweet array
+  	for (var i = 0; i < tweets.length; i++) {
+  		console.log(tweets[i].text);
+  		}
+    }
   else 
   	console.log(error);
 
+	});
+};
+
+var manyMovies = function(theMovies) {
+
+	if (theMovies === undefined) {
+		theMovies = "Mr Nobody";
+	}
+
+request('http://www.omdbapi.com/?t=' + theMovies + '&y=&plot=short&tomatoes=true&r=json&apikey=40e9cece', function (error, response, body) {
+ if (!error && response.statusCode === 200){
+ 	var jsonData = JSON.parse(body);
+ 	console.log("Title: " + jsonData.Title);
+ 	console.log("Actors: " + jsonData.Actors);
+ 	console.log("Plot: " + jsonData.Plot);
+    console.log("Year: " + jsonData.Year);
+    console.log("Rated: " + jsonData.Rated);
+    console.log("IMDB Rating: " + jsonData.imdbRating);
+    console.log("Country: " + jsonData.Country);
+    console.log("Language: " + jsonData.Language);
+ 	}
+  });
+};
+
+// Spotify function
+var mySpotify = function(myMusic) {
+var spotify = new Spotify({
+	id: "cb1e7dd2d4f540a397f41bcf753c4619",
+	secret: "89cf8d5982b544c5ac9b42966cb9d5dc"
 });
-	// // client.get("statuses/user_timeline", params, function(error, tweets, response) {
-	// 	if (!error) {
-	// 		for (var i = 0; i < tweets.length; i++) {
-	// 			console.log(tweets[i]);
-				
-	// 		}
-	// 	}
-	// });
-};
 
-//Spotify function
-function mySpotify(music) {
-	if (music === undefined) {
-		music = "The Sign";
+spotify.search ({ type: 'track', query: myMusic, limit: 1 }, function(err, data) {
+	if (err) {
+		return console.log('Error occurred: ' + err);
 	}
-	mySpotify.search({type: "track", query: music}, function(err, data) {
-		if (err) {
-			return console.log("Error occurred: " + err);
-		}
+	console.log(data);
 
-		var song = data.tracks.items;
+	var music = data.tracks.items;
 
-		for (var i = 0; i < song.length; i++) {
-			console.log("Artist(s): " + song[i].artists.map(getArtistNames));
-			console.log("Song Name: " + song[i].name);
-			console.log("Preview Song: " + song[i].preview_url);
-			console.log("Album: " + song[i].album.name);
-		}
-	});
-};
-
-// Movie search function
-function findMovie(myMovie) {
-	if (myMovie === undefined) {
-		myMovie = "Mr Nobody";
+	for (var i = 0; i < music.length; i++) {
+		console.log("artist(s): " + music[i].artist);
+      	console.log("song name: " + music[i].name);
+      	console.log("preview song: " + music[i].preview_url);
+      	console.log("album: " + music[i].album.name);
 	}
-	var queryUrl = "http://www.omdbapi.com/?t=" + myMovie + "&y=&plot=short&tomatoes=true&r=json&apikey=40e9cece";
-	request(queryUrl, function(error, response, body) {
-		if (!error && response.statusCode === 200) {
-			var movieData = JSON.parse(body);
-
-			console.log("Title: " + movieData.Title);
-			console.log("Year: " + movieData.Year);
-			console.log("IMDB Rating: " + movieData.imdbRating);
-			console.log("Rotten Tomatoes Rating: " + movieData.tomatoRating);
-			console.log("Country: " + movieData.Country);
-			console.log("Language: " + movieData.Language);
-			console.log("Plot: " + movieData.Plot);
-			console.log("Actors: " + movieData.Actors);
-		}
-	});
+});
 };
+//  Command function
+var choices = function(requestData, askData) {
+	switch (requestData) {
+		case "my-Tweets":
+		 console.log("");
+		myTweets();
+		break;
+		case "movie-this":
+		manyMovies(askData);
+		break;
+		case "spotify-this-song":
+		mySpotify(askData);
 
-// Function for liri to read from random text file
-function okLiri() {
-	fs.readFile("random.txt", "utf8", function(error, data) {
-		if (error) {
-			return console.log(error);
-		}
-		console.log(data);
-		var dataArr = data.split(",");
-		
-	});
+	}
 };
 
 // To take arguments
-var request = process.argv[2];
-var execute = process.argv[3];
-
-//  Command function
-	switch (request) {
-		case "myTweets":
-		 console.log("myTweets");
-		myTweets();
-		break;
-		case "mySpotify":
-		 console.log("mySpotify");
-		mySpotify();
-		break;
-		case "findMovie":
-		findMovie();
-		break;
-		case "ok":
-		okLiri();
-		break;
-		default:
-			console.log("liri doesn't know that");
-	}
-
+var myArguments = function(argOne, argTwo) {
+	choices(argOne, argTwo);
+};
+myArguments(process.argv[2], process.argv[3]);
